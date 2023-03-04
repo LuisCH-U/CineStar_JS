772d7ecc8b660a97a9cd7de9c45db3659158cd43
LuisCH <luis13chipana@gmail.com>
2023-03-04 00:14:33 -0500

    CineStar_JS

---
+++ js/peliculas.js
@@ -0,0 +1,35 @@
+const idx = (new URLSearchParams (window.location.search)).get('idx');
+const getPeliculas = async () => {
+    const data = await fetch(`http://localhost/cinestar_sweb/peliculas/${idx}`);
+    if ( data.status === 200 ) {
+        const peliculas = await data.json();
+        let html = `<br/><h1>Cartelera</h1><br/>`;
+
+        peliculas.forEach(pelicula => {
+            html += `
+            <div class="contenido-pelicula">
+                <div class="datos-pelicula">
+                    <h2>${pelicula.Titulo}</h2><br/>
+                    <p>${pelicula.Sinopsis}</p>
+                    <br/>
+                    <div class="boton-pelicula"> 
+                        <a href="pelicula.html?idd=${pelicula.id}" >
+                            <img src="img/varios/btn-mas-info.jpg" width="120" height="30" alt="Ver info"/>
+                        </a>
+                    </div>
+                    <div class="boton-pelicula"> 
+                        <a href="https://www.cinestar.com.pe/popups/trailer/${pelicula.Link}" onclick="return hs.htmlExpand(this, { objectType: 'iframe' } )" >
+                            <img src="img/varios/btn-trailer.jpg" width="120" height="30" alt="Ver trailer"/>
+                        </a>
+                    </div> 
+                </div>
+                <img src="img/pelicula/${pelicula.id}.jpg" width="160" height="226"/><br/><br/>
+             </div>  
+            `
+        });
+        document.getElementById('contenido-interno').innerHTML = html;
+    }
+}
+
+
+getPeliculas()
