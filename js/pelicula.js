5a780c60b1f250180779f6b0d8f553fbf298e07c
LuisCH-U <126347548+LuisCH-U@users.noreply.github.com>
2023-03-04 01:15:35 -0500

    Update pelicula.js

--- js/pelicula.js
+++ js/pelicula.js
@@ -1,101 +1,51 @@
 
-const getCinePelicula = async () => {
-    const data = await fetch(`http://localhost/cinestar_sweb/pelicula/${idx}`);
-    if ( data.status === 200 ) {
-        const Pelicula = await data.json();
-
-        let html = `<h2>${pelicula.RazonSocial}</h2>`;
-        Pelicula.forEach(pelicula => {
-            html += `
-			<div class="contenido-pelicula">
-			<div class="datos-pelicula">
-				<h2>${pelicula.Titulo}</h2>
-				<p>${pelicula.Sinopsis}</p>
-				<br/>
-				<div class="tabla">
-					<div class="fila">
-						<div class="celda-titulo">Título Original :</div>
-						<div class="celda">${pelicula.Titulo}</div>
-					</div>
-					<div class="fila">
-						<div class="celda-titulo">Estreno :</div>
-						<div class="celda">${pelicula.FechaEstreno}</div>
-					</div>
-					<div class="fila">
-						<div class="celda-titulo">Género :</div>
-						<div class="celda">${pelicula.Generos}</div>
-					</div>
-					<div class="fila">
-						<div class="celda-titulo">Director :</div>
-						<div class="celda">${pelicula.Director}</div>
-					</div>
-					<div class="fila">
-						<div class="celda-titulo">Reparto :</div>
-						<div class="celda">${pelicula.Reparto}</div>
-					</div>
-				</div>
-			</div>
-			<img src="img/pelicula/${pelicula.id}.jpg" width="160" height="226"><br/><br/>
-		</div>
-		<div class="pelicula-video">
-			<!-- <iframe width="580" height="400" src="http://www.youtube.com/v/6maujJFcuxA" frameborder="0" allowfullscreen></iframe> -->
-			<embed src="http://www.youtube.com/v/${pelicula.Link}" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="580" height="400">
-		</div>
-            `
-        });
-        document.getElementById('contenido-interno').innerHTML = html;
-    }
-}
-
-
-getCinePelicula()
-
-
-/*
 const getPeliculas = async () => {
-    const data = await fetch(`http://localhost/cinestar_sweb/peliculas/${idx}`);
+    const data = await fetch(`http://localhost/cinestar_sweb/peliculas`);
     if ( data.status === 200 ) {
-        const Pelicula = await data.json();
+        const peliculas = await data.json();
+        let html = `<br/><h1>Cartelera</h1><br/>`;
+
         
-        Pelicula.forEach(Pelicula => {
             html += `
             <div class="contenido-pelicula">
-					<div class="datos-pelicula">
-						<h2>${Pelicula.Titulo}</h2>
-						<p>${Pelicula.Sinopsis}</p>
-						<br/>
-						<div class="tabla">
-							<div class="fila">
-								<div class="celda-titulo">Título Original :</div>
-								<div class="celda">${Pelicula.Titulo}</div>
-							</div>
-							<div class="fila">
-								<div class="celda-titulo">Estreno :</div>
-								<div class="celda">${Pelicula.FechaEstreno}</div>
-							</div>
-							<div class="fila">
-								<div class="celda-titulo">Género :</div>
-								<div class="celda">${Pelicula.Generos}</div>
-							</div>
-							<div class="fila">
-								<div class="celda-titulo">Director :</div>
-								<div class="celda">${Pelicula.Director}</div>
-							</div>
-							<div class="fila">
-								<div class="celda-titulo">Reparto :</div>
-								<div class="celda">${Pelicula.Reparto}</div>
+						<div class="datos-pelicula">
+							<h2>Jumanji: En la Selva (Todo Público)</h2>
+							<p>Remake de la película homónima de 1995 adaptado a la época actual, en donde cuatro adolescentes se introducen en un nueva aventura a partir de “Jumanji”, un videojuego que sirve como un portal a través del espacio-tiempo. Absorbidos por el mundo de Jumanji, este juego no se puede abandonar hasta que acaba la partida</p>
+							<br>
+							<div class="tabla">
+								<div class="fila">
+									<div class="celda-titulo">Título Original :</div>
+									<div class="celda">Jumanji: En la Selva (Todo Público)</div>
+								</div>
+								<div class="fila">
+									<div class="celda-titulo">Estreno :</div>
+									<div class="celda">11 de Enero del 2018</div>
+								</div>
+								<div class="fila">
+									<div class="celda-titulo">Género :</div>
+									<div class="celda">Aventura / Acción</div>
+								</div>
+								<div class="fila">
+									<div class="celda-titulo">Director :</div>
+									<div class="celda">Jake Kasdan</div>
+								</div>
+								<div class="fila">
+									<div class="celda-titulo">Reparto :</div>
+									<div class="celda">Dwayne Johnson, Kevin Hart, Jack Black, Karen Gillan</div>
+								</div>
 							</div>
 						</div>
+						<img src="img/pelicula/1.jpg" width="160" height="226"><br><br>
 					</div>
-					<img src="img/pelicula/${Pelicula.id}.jpg" width="160" height="226"><br/><br/>
-				</div>
-				<div class="pelicula-video">
-					<!-- <iframe width="580" height="400" src="http://www.youtube.com/v/6maujJFcuxA" frameborder="0" allowfullscreen></iframe> -->
-					<embed src="http://www.youtube.com/v/${pelicula.Link}" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="580" height="400">
-				</div>
+					<div class="pelicula-video">
+						 
+						<embed src="https://www.youtube.com/v/2QKg5SZ_35I" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="580" height="400">
+					</div>
             `
-        });
+        
         document.getElementById('contenido-interno').innerHTML = html;
     }
 }
-*/
+
+
+getPeliculas()
